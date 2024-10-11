14a97f2e9cc991cbb6adc0b76c72b3dc4e4ed700
Anthony <anthony.artavia20@gmail.com>
2024-10-11 09:38:49 -0600

    BST InOrder, PreOrder,PostOrder

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using BSTProgram;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        BinarySearchTree ejemplo = new BinarySearchTree();
+
+        ejemplo.insert(5);
+        ejemplo.insert(1);
+        ejemplo.insert(2);
+        ejemplo.insert(4);
+        ejemplo.insert(3);
+
+        Console.WriteLine("InOrder: ");
+        ejemplo.inOrder();
+        Console.WriteLine("PreOrder: ");
+        ejemplo.preOrder();
+        Console.WriteLine("PostOrder: ");
+        ejemplo.postOrder();
+    }
+}
---
+++ TreeNode.cs
@@ -0,0 +1,15 @@
+namespace BSTProgram
+{
+    class TreeNode 
+    {
+        public int? key;
+        public TreeNode? left, right;
+
+        public TreeNode(int item) {
+            key = item;
+            left = right = null;
+        }
+
+        
+    }
+}
